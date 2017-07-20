dffdb0e8c617c038c49b3228f4e451c63ca51386
PJWonnnn <nana8116@gmail.com>
2017-07-20 16:38:10 +0900

    git test

--- app/src/main/java/example/com/smu_1_demo/MainActivity.java
+++ app/src/main/java/example/com/smu_1_demo/MainActivity.java
@@ -7,13 +7,14 @@ import android.util.Log;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private  Log;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG,"f");
+        Log.d(TAG,"test2");
+
     }
 
 
