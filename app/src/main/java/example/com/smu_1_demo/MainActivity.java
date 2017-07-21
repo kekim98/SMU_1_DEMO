f4a994de01c34937817bebb4039aed1d33b16750
tkdaudeo17 <tkdaudeo17@gmail.com>
2017-07-21 13:24:15 +0900

    이정우

--- app/src/main/java/example/com/smu_1_demo/MainActivity.java
+++ app/src/main/java/example/com/smu_1_demo/MainActivity.java
@@ -1,7 +1,7 @@
 package example.com.smu_1_demo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d(TAG,"f");
         Log.d(TAG,"FINALLLL");
+        Log.d(TAG,"No");
 
     }
 
