5fd2deff029908fd0ece1b62f146d0ceade0ef40
201721144 <audtjq213@gmail.com>
2017-07-21 11:50:37 +0900

    test1

--- app/src/main/java/example/com/smu_1_demo/MainActivity.java
+++ app/src/main/java/example/com/smu_1_demo/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG,"f");
-        Log.d(TAG,"test2");
+        Log.d(TAG,"test4");
 
     }
 
